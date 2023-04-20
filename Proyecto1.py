25ad9a6bc905360ba882f8116330d38821443e89
Pil <pfdez2017@gmail.com>
2023-04-20 00:11:40 -0600

    Se completó opc 1: registrar nuevos libros, opc 2: registrar nuevas revistas,  opc 3 mostrar libros y revistas Se limpió el resto del código

--- Proyecto1.py
+++ Proyecto1.py
@@ -9,53 +9,29 @@ def clear():
 
 from mis_clases import *
 
-
-# Almacenar datos de Libros
-def datos_libros():
-
-# Almacena datos de Usuarios
-def datos_usuarios():
-    # completar datos de usuarios
-
+'''
 
 def datosServicios():
     #inicializar datos servicios
     servicio1.cedula_usu = "27373737"
     servicio1.tipo_serv = "Prestamo"
     servicio1.nom_lib = "Hola"
-
+'''
 def limpiar_pant():
     for a in range(30):
         print(" ")
 
 # MAIN
 if __name__ == "__main__":
-    libro1 = Libro()        # instanciar
-    libro2 = Libro()
-    libro3 = Libro()
-
-    usuario1 = Usuario()
-    usuario2 = Usuario()
-    usuario3 = Usuario()
-
-    servicio1 = Servicio()
-    servicio2 = Servicio()
-    servicio3 = Servicio()
-    servicio4 = Servicio()
-
     s = Servicio()
     u = Usuario()
-    l = Libro()
 
-    datos_libros()
-    # datos de los libros existentes
-    datos_usuarios()
-    # datos de los usuarios existentes
-    datosServicios()
-    # datos de los servicios asignados
 
-    r = Revista()
-    arrObjs = []  # vector de objetos
+    l = Libro()         # Instanciar Libros clase heredada de clase Publicaciones
+    r = Revista()       # Instanciar Revista, clase heredada de clase Publicaciones
+    arrObjs = []        # vector de objetos para almacenar libros y revistas
+    arrRev = []
+
     opc = 1
     while opc != 0:
         limpiar_pant()
@@ -69,11 +45,11 @@ if __name__ == "__main__":
         print("                                           ")
         print("             MENU PRINCIPAL                ")
         print("                                           ")
-        print("    OPC 1: Mostrar usuarios                ")
-        print("    OPC 2: NUEVOS  usuarios                ")
+        print("    OPC 1: Registrar Nuevos Libros         ")
+        print("    OPC 2: Registrar Nuevas Revistas       ")
         print("                                           ")
-        print("    OPC 3: Mostrar libros                  ")
-        print("    OPC 4: NUEVOS libros                   ")
+        print("    OPC 3: Mostrar Publicaciones           ")
+        print("                                           ")
         print("                                           ")
         print("    OPC 5: Mostrar servicio                ")
         print("    OPC 6: NUEVOS servicios                ")
@@ -89,67 +65,45 @@ if __name__ == "__main__":
             # no digitó un número
             control = input("Error al digitar la opción, Presione ENTER para continuar ==>")
             continue
-        elif opc == "1":                  # Opc 1 = Mostrar usuarios registrados
-            print("            Usuarios del Sistema            ")
-            u = usuario1
-            print(u.mostrarUsuario())
-            u = usuario2
-            print(u.mostrarUsuario())
-            u = usuario3
-            print(u.mostrarUsuario())
+        elif opc == "1":                  # Opc 1 = Registrar nuevos libros
+            print("            Registrar NUEVOS Libros               ")
+            z = int(input("Cúantos libros desea registrar: "))
+            for i in range(z):
+                print("Ingresando libro ",i+1)
+                x = str("obj") + str(i)
+                x = Libro()
+                x.setDatos()
+                arrObjs.append(x)
             print()
-        elif opc == "2":                  # Opc 2 = incluir nuevos usuarios
-            print("            NUEVOS Usuarios               ")
-            u.nuevoUsuario()       # agrega nuevo usuario
-            print()
-            print(u.mostrarUsuario())
+        elif opc == "2":                  # Opc 2 = Registrar nuevas Revistas
+            print("            Registrar NUEVAS Revistas             ")
+            z = int(input("Cúantas Revistas desea registrar: "))
+            for i in range(z):
+                print("Ingresando Revista ", i+1)
+                y = str("obj") + str(i)
+                y = Revista()
+                y.setDatos()
+                arrRev.append(y)
             print()
 
         elif opc == "3":
-            # Mostrar Libros registrados
-            print("            Mostrar Libros             ")
-            print()
-            for j in range(3):
-                print("Indice: ", j)
+            # Mostrar Publicaciones registradas
+            print("            Mostrar Publicaciones        ")
+            print("Libros Registrados")
+            for j in range(len(arrObjs)):
                 print(arrObjs[j].autor, " ", arrObjs[j].titulo)
-
-           ''' l = libro1
-            print(l.mostrarLibro())
-            l = libro2
-            print(l.mostrarLibro())
-            l = libro3
-            print(l.mostrarLibro())
-            print()'''
+            print()
+            print("Revistas Registradas")
+            for i in range(len(arrRev)):
+                print(arrRev[i].autor, " ", arrRev[i].titulo)
+            print()
 
         elif opc == "4":
-            print("            NUEVOS Libros               ")
+            print("            Solicitar Préstamo               ")
             print()
-
-
-
-            for i in range(3):
-                x = str("obj") + str(i)
-                x = Revista()
-                x.setDatos()
-                arrObjs.append(x)
-                # print(f"Indice {i} dato {arrObjs[i].autor}")
-
-
-            '''l.nuevoLibro()
-            print()
-            print(l.nuevoLibro())'''
 
         elif opc == "5":
-            print("            Mostrar Servicios           ")
-            print()
-            s = servicio1
-            print(s.mostrarServicio())
-            s = servicio2
-            print(s.mostrarServicio())
-            s = servicio3
-            print(s.mostrarServicio())
-            s = servicio4
-            print(s.mostrarServicio())
+            print("            Solicitar Devolución           ")
             print()
 
         elif opc == "6":
@@ -197,7 +151,7 @@ if __name__ == "__main__":
         elif opc == "8":
             # Registrar ingresos por prestación de servicios
             result = 0
-            matr1 = np.array([0,1,2,3])
+            matr1 = [0,1,2,3]
             for i in range(4):
                 matr1[i] = float(input(f"Digite los ingresos #{i+1} $:"))
                 result += matr1[i]
